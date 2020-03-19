e30128949f0f82c0fa3ca604d08023d849258720
abdulaziz <alzaabi98@gmail.com>
2020-03-19 08:29:55 +0400

    adding provider and link it with main basker counter

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:fluttershoppingcart/models/cart.dart';
 import 'package:fluttershoppingcart/models/item.dart';
 import 'package:fluttershoppingcart/screens/checkout_page.dart';
+import 'package:provider/provider.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(ChangeNotifierProvider(
+    create: (context) => Cart(),
+    child: MyApp(),
+  ));
+}
 
 class MyApp extends StatelessWidget {
   @override
@@ -27,41 +34,45 @@ class _HomePageState extends State<HomePage> {
   ];
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Shopping cart'),
-        actions: <Widget>[
-          Padding(
-            padding: EdgeInsets.all(8.0),
-            child: Row(
-              children: <Widget>[
-                IconButton(
-                  icon: Icon(
-                    Icons.shopping_cart,
-                    color: Colors.white,
+    return Consumer<Cart>(builder: (context, cart, child) {
+      return Scaffold(
+        appBar: AppBar(
+          title: Text('Shopping cart'),
+          actions: <Widget>[
+            Padding(
+              padding: EdgeInsets.all(8.0),
+              child: Row(
+                children: <Widget>[
+                  IconButton(
+                    icon: Icon(
+                      Icons.shopping_cart,
+                      color: Colors.white,
+                    ),
+                    onPressed: () {
+                      Navigator.of(context).push(MaterialPageRoute(
+                          builder: (context) => CheckoutPage()));
+                    },
                   ),
-                  onPressed: () {
-                    Navigator.of(context).push(MaterialPageRoute(
-                        builder: (context) => CheckoutPage()));
-                  },
-                ),
-                Text('3')
-              ],
-            ),
-          )
-        ],
-        centerTitle: true,
-      ),
-      body: ListView.builder(
-        itemCount: items.length,
-        itemBuilder: (context, index) {
-          return ListTile(
-            title: Text(items[index].title),
-            trailing: Icon(Icons.add),
-            onTap: () {},
-          );
-        },
-      ),
-    );
+                  Text(cart.count.toString())
+                ],
+              ),
+            )
+          ],
+          centerTitle: true,
+        ),
+        body: ListView.builder(
+          itemCount: items.length,
+          itemBuilder: (context, index) {
+            return ListTile(
+              title: Text(items[index].title),
+              trailing: Icon(Icons.add),
+              onTap: () {
+                cart.add(items[index]);
+              },
+            );
+          },
+        ),
+      );
+    });
   }
 }
--- lib/models/cart.dart
+++ lib/models/cart.dart
@@ -1,17 +1,20 @@
+import 'package:flutter/material.dart';
 import 'package:fluttershoppingcart/models/item.dart';
 
-class Cart {
+class Cart extends ChangeNotifier {
   List<Item> _items = [];
   double _totalPrice = 0.0;
 
   void add(Item item) {
     _items.add(item);
     _totalPrice += item.price;
+    notifyListeners();
   }
 
   void remove(Item item) {
     _totalPrice -= item.price;
     _items.remove(item);
+    notifyListeners();
   }
 
   int get count {
